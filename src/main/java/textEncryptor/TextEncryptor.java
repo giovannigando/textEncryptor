e69d8d2b35cf375f857ad213423ee7304fd86f80
giovannigando <giovannigandov@hotmail.com>
2022-03-05 12:31:47 +0100

    Creation of right configuration settings for an Eclipse project.

---
+++ src/main/java/textEncryptor/TextEncryptor.java
@@ -0,0 +1,10 @@
+package textEncryptor;
+
+public class TextEncryptor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the text encryptor");
+	}
+
+}
